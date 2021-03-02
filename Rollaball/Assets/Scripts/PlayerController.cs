ea9b6c2795053073b1eae5845077528fd9c1b4d5
TheBlueFish116 <48139732+TheBlueFish116@users.noreply.github.com>
2021-03-01 21:25:10 -0700

    Added Rollaball

---
+++ Rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class PlayerController : MonoBehaviour
+{
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public GameObject loseTextObject;
+    public float speed = 0;
+
+    private Rigidbody rb;
+    private int count;
+    private float movementx;
+    private float movementy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+    
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementx = movementVector.x;
+        movementy = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 13)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3(movementx, 0.0f, movementy);
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("void"))
+        {
+            winTextObject.SetActive(false);
+            loseTextObject.SetActive(true);
+        }
+        if (other.gameObject.CompareTag("PickUp")) {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+}
---
+++ Rollaball/Assets/Scripts/Rotator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.CompareTag("PickUp"))
+        {
+            transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+        }else if (gameObject.CompareTag("zRotate"))
+        {
+            transform.Rotate(new Vector3(0, 0, -30) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(-30, 0, 0) * Time.deltaTime);
+        }
+    }
+}
