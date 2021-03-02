354cca77ab72ebfd8d430a1be9b813dfdc615ed3
TheBlueFish116 <48139732+TheBlueFish116@users.noreply.github.com>
2021-03-01 21:08:17 -0700

    flappy-bird
    added flappy-bird

---
+++ Flappy-Bird/Assets/FlappyBirdStyleAssets/Scripts/CoinHandler.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinHandler : MonoBehaviour
+{
+    public GameObject coin;
+    public AudioClip coinsound;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+
+        GameControl.instance.BirdScored();
+        AudioSource.PlayClipAtPoint(coinsound, other.transform.position);
+        Destroy(coin);
+
+    }
+}
---
+++ Flappy-Bird/Assets/FlappyBirdStyleAssets/Scripts/ColumnPool.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColumnPool : MonoBehaviour
+{
+    public GameObject columnPrefab;
+    public int columnPoolSize = 200;
+    public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3.5f;
+    public int numOfColumns;
+    public float difficultIncrease = 1.0f;
+
+    private GameObject[] columns;
+    private int currentColumn = 0;
+
+    private Vector2 objectPoolPosition = new Vector2(-15, -25);
+    private float spawnXPosition = 10f;
+
+    private float timeSinceLastSpawned;
+
+
+    void Start()
+    {
+        timeSinceLastSpawned = 0f;
+        columns = new GameObject[columnPoolSize];
+        for (int i = 0; i < columnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            numOfColumns++;
+            if(numOfColumns == 5)
+            {   if (spawnRate >= 1f)
+                {
+                    spawnRate = spawnRate - 1.0f;
+                }
+                numOfColumns = 0;
+                difficultIncrease = difficultIncrease - 1.0f;
+            }
+            timeSinceLastSpawned = 0f;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
